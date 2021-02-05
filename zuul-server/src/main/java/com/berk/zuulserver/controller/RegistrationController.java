045be41f44b1684af7f0fd2ac4ce3bbea1534e01
sberk97 <sebastian.berk97@gmail.com>
2021-02-05 01:18:23 +0100

    Removed unused imports

--- zuul-server/src/main/java/com/berk/zuulserver/controller/RegistrationController.java
+++ zuul-server/src/main/java/com/berk/zuulserver/controller/RegistrationController.java
@@ -1,17 +1,16 @@
 package com.berk.zuulserver.controller;
 
-import com.berk.zuulserver.service.MyUserDetailsService;
 import com.berk.zuulserver.model.RegisterUser;
+import com.berk.zuulserver.service.MyUserDetailsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
