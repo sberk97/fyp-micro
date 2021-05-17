9675ba63b4933766de67730695b07fa24f070c01
sberk97 <sebastian.berk97@gmail.com>
2021-05-17 23:32:34 +0200

    Implement jwt token service

--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { BackendService } from 'src/app/services/backend.service';
+import { JWTTokenService } from 'src/app/utils/jwt.token.service';
 
 @Component({
   selector: 'app-login',
@@ -19,7 +20,8 @@ export class LoginComponent implements OnInit {
   constructor(
     private router: Router,
     private backendService: BackendService,
-    private cookieService: CookieService
+    private cookieService: CookieService,
+    private jwtTokenService: JWTTokenService
   ) {}
 
   ngOnInit(): void {
@@ -36,6 +38,11 @@ export class LoginComponent implements OnInit {
         // this.cookieService.set('username', response.user, 365, '/');
         this.cookieService.set('jwt', response.jwt, 1, '/');
         void this.router.navigate(['/']);
+        this.jwtTokenService.setToken(response.jwt);
+        console.log(this.jwtTokenService.getUser());
+        console.log(this.jwtTokenService.getRoles());
+        console.log(this.jwtTokenService.getExpiryTime());
+        console.log(this.jwtTokenService.isTokenExpired());
       },
       (error: HttpErrorResponse) => {
         this.loginFailed = true;
---
+++ front-end/src/app/utils/jwt.token.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JWTTokenService {
+  jwtToken!: string;
+  decodedToken!: { [key: string]: string };
+
+  public setToken(token: string): void {
+    if (token) {
+      this.jwtToken = token;
+    }
+  }
+
+  public decodeToken(): void {
+    if (this.jwtToken) {
+      this.decodedToken = jwt_decode(this.jwtToken);
+    }
+  }
+
+  public getDecodeToken(): string {
+    return jwt_decode(this.jwtToken);
+  }
+
+  public getUser(): string {
+    this.decodeToken();
+    return this.decodedToken ? this.decodedToken.sub : '';
+  }
+
+  public getRoles(): string {
+    this.decodeToken();
+    return this.decodedToken ? this.decodedToken.roles : '';
+  }
+
+  public getExpiryTime(): number | string {
+    this.decodeToken();
+    return this.decodedToken ? this.decodedToken.exp : -1;
+  }
+
+  public isTokenExpired(): boolean {
+    const expiryTime: number = this.getExpiryTime() as number;
+    if (expiryTime) {
+      return 1000 * expiryTime - new Date().getTime() < 5000;
+    } else {
+      return false;
+    }
+  }
+}
