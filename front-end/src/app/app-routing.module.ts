fff789a32a7a074ad5886d3a19e52d8c7cb3c0e3
sberk97 <sebastian.berk97@gmail.com>
2021-05-18 20:51:03 +0200

    Remove additional folder added accidently

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdvertPageComponent } from './components/advert-page/advert-page.component';
 import { LoginComponent } from './components/login/login.component';
-import { NotFoundPageComponent } from './components/not-found-page/not-found-page/not-found-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
