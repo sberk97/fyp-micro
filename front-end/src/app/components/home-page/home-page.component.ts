c4afd2b7bd83b3af164544cd53d6ff66a701568e
sberk97 <sebastian.berk97@gmail.com>
2021-05-19 17:49:50 +0200

    Moved search bar to separate component

--- front-end/src/app/components/home-page/home-page.component.ts
+++ front-end/src/app/components/home-page/home-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import * as Base64 from 'js-base64';
 import { Advert } from 'src/app/models/advert/advert';
 import { BackendService } from 'src/app/services/backend/backend.service';
 
@@ -11,20 +9,13 @@ import { BackendService } from 'src/app/services/backend/backend.service';
 export class HomePageComponent implements OnInit {
   advertList!: Advert[];
   advertListLoaded!: Promise<boolean>;
-  searchQuery!: string;
 
-  constructor(private backendService: BackendService, private router: Router) {}
+  constructor(private backendService: BackendService) {}
 
   ngOnInit(): void {
     this.backendService.getLastNAdverts(5).subscribe((response: Advert[]) => {
       this.advertList = response;
       this.advertListLoaded = Promise.resolve(true);
     });
-  }
-
-  onSubmit(): void {
-    if (this.searchQuery.length > 0) {
-      void this.router.navigate(['search/' + Base64.encode(this.searchQuery)]);
-    }
   }
 }
---
+++ front-end/src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import * as Base64 from 'js-base64';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+})
+export class SearchBarComponent {
+  @Input() searchQuery!: string;
+
+  constructor(private router: Router) {}
+
+  onSubmit(): void {
+    if (this.searchQuery.length > 0) {
+      void this.router.navigate(['search/' + Base64.encode(this.searchQuery)]);
+    }
+  }
+}
