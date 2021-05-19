5ffdd10711f238556d6db5b329ad99ad8af9e572
sberk97 <sebastian.berk97@gmail.com>
2021-05-19 02:32:39 +0200

    Implemented error message when no results found, check promise before displaying adverts

--- front-end/src/app/components/search-page/search-page.component.ts
+++ front-end/src/app/components/search-page/search-page.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import * as Base64 from 'js-base64';
 import { Advert } from 'src/app/models/advert/advert';
 import { BackendService } from 'src/app/services/backend/backend.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   templateUrl: './search-page.component.html',
@@ -19,15 +20,28 @@ export class SearchPageComponent implements OnInit {
   advertList!: Advert[];
   advertListLoaded!: Promise<boolean>;
   query!: string;
+  searchFailed = false;
+  failedMsg!: string;
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.query = Base64.decode(params['query']);
       if (this.query) {
-        this.backendService.getAdvertByTiitle(this.query).subscribe((data) => {
-          this.advertList = data;
-          this.advertListLoaded = Promise.resolve(true);
-        });
+        this.backendService.getAdvertByTiitle(this.query).subscribe(
+          (data) => {
+            this.advertList = data;
+            this.advertListLoaded = Promise.resolve(true);
+          },
+          (error: HttpErrorResponse) => {
+            this.searchFailed = true;
+            console.log('here');
+            if (error.status === 404) {
+              this.failedMsg = 'No results for your search';
+            } else {
+              this.failedMsg = 'Could not fetch results';
+            }
+          }
+        );
       } else {
         void this.router.navigate(['not-found']);
       }
