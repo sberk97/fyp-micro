e3022db5a00a1365fd2cbf1d933ed3d6efd4019e
sberk97 <sebastian.berk97@gmail.com>
2021-05-21 01:25:45 +0200

    add advert page cleanup

--- front-end/src/app/components/add-advert-page/add-advert-page.component.ts
+++ front-end/src/app/components/add-advert-page/add-advert-page.component.ts
@@ -1,13 +1,16 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { BackendService } from 'src/app/services/backend/backend.service';
 
 @Component({
   templateUrl: './add-advert-page.component.html',
   styleUrls: ['./add-advert-page.component.scss'],
 })
-export class AddAdvertPageComponent {
+export class AddAdvertPageComponent implements OnDestroy {
+  private subscription: Subscription = new Subscription();
+
   title!: string;
   price!: number;
   description!: string;
@@ -18,6 +21,10 @@ export class AddAdvertPageComponent {
 
   constructor(private router: Router, private backendService: BackendService) {}
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   onSubmit(): void {
     const body = {
       title: this.title,
@@ -25,20 +32,22 @@ export class AddAdvertPageComponent {
       contact_details: this.contactDetails,
       price: this.price,
     };
-    this.backendService.addAdvert(body).subscribe(
-      (response: number) => {
-        void this.router.navigate(['/advert/' + response.toString()]);
-      },
-      (error: HttpErrorResponse) => {
-        this.postFailed = true;
-        if (error.status === 400) {
-          this.failedMsg = error.error as string;
-        } else if (error.status === 401) {
-          void this.router.navigate(['/login']);
-        } else {
-          this.failedMsg = 'Adding advert failed!';
+    this.subscription.add(
+      this.backendService.addAdvert(body).subscribe(
+        (response: number) => {
+          void this.router.navigate(['/advert/' + response.toString()]);
+        },
+        (error: HttpErrorResponse) => {
+          this.postFailed = true;
+          if (error.status === 400) {
+            this.failedMsg = error.error as string;
+          } else if (error.status === 401) {
+            void this.router.navigate(['/login']);
+          } else {
+            this.failedMsg = 'Adding advert failed!';
+          }
         }
-      }
+      )
     );
   }
 }
