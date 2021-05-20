0555979484e0eba21e4b50934e9965f8c8453edb
sberk97 <sebastian.berk97@gmail.com>
2021-05-21 01:24:07 +0200

    advert page cleanup

--- front-end/src/app/components/advert-page/advert-page.component.ts
+++ front-end/src/app/components/advert-page/advert-page.component.ts
@@ -11,8 +11,8 @@ import { JWTTokenService } from 'src/app/services/jwt/jwt.token.service';
   styleUrls: ['./advert-page.component.scss'],
 })
 export class AdvertPageComponent implements OnInit, OnDestroy {
-  private routeSub!: Subscription;
-  advertLoaded!: Promise<boolean>;
+  private subscription: Subscription = new Subscription();
+  public advertLoaded!: Promise<boolean>;
 
   loadingFailed = false;
   failedMsg = '';
@@ -27,28 +27,40 @@ export class AdvertPageComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.routeSub = this.route.params.subscribe((params) => {
-      this.id = params['id'] as number;
-    });
+    this.getIdFromPath();
 
-    this.backendService.getAdvert(this.id).subscribe(
-      (response: Advert) => {
-        this.advert = response;
-        this.advertLoaded = Promise.resolve(true);
-      },
-      (error: HttpErrorResponse) => {
-        this.loadingFailed = true;
-        if (error.status === 404) {
-          // redirect to 404 page
-          void this.router.navigate(['not-found']);
-        } else {
-          this.failedMsg = 'Fetching advert failed';
+    this.getAdvert();
+  }
+
+  private getIdFromPath(): void {
+    this.subscription.add(
+      this.route.params.subscribe((params) => {
+        this.id = params['id'] as number;
+      })
+    );
+  }
+
+  private getAdvert(): void {
+    this.subscription.add(
+      this.backendService.getAdvert(this.id).subscribe(
+        (response: Advert) => {
+          this.advert = response;
+          this.advertLoaded = Promise.resolve(true);
+        },
+        (error: HttpErrorResponse) => {
+          this.loadingFailed = true;
+          if (error.status === 404) {
+            // redirect to 404 page
+            void this.router.navigate(['not-found']);
+          } else {
+            this.failedMsg = 'Fetching advert failed';
+          }
         }
-      }
+      )
     );
   }
 
   ngOnDestroy(): void {
-    this.routeSub.unsubscribe();
+    this.subscription.unsubscribe();
   }
 }
