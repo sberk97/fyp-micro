792a86c6925f0f9884d9ab5e35cb3ebae03d2eba
sberk97 <sebastian.berk97@gmail.com>
2021-05-21 17:56:26 +0200

    Revert changes that were trying to introduce API_URL

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 export const environment = {
   production: true,
-  apiUrl: window['env']['apiUrl'] || 'default',
 };
